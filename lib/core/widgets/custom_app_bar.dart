1201ac08133ad91c422387fccb836d09c911718f
Mohamed Ezzat <MohamedEzzat5>
2024-05-06 19:15:13 +0300

    create custom appbar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,29 +1,56 @@
-// import 'package:flutter/material.dart';
-//
-// import '../utils/assets_manger.dart';
-//
-//
-// class CustomAppBar extends StatelessWidget {
-//   const CustomAppBar({super.key,required this.appBarTitle});
-//   final String appBarTitle;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.white,
-//       child: Padding(
-//         padding: const EdgeInsets.only(top: 52.5,right: 20,left: 20,bottom: 12.5),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           children: [
-//             Image.asset(
-//               AssetsData.logo,
-//               width: 85,
-//             ),
-//             const Spacer(),
-//             Text(appBarTitle,style: Styles.textStyle20.copyWith(color: kPrimaryColor,fontWeight: FontWeight.w600),),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+import '../utils/constance.dart';
+import '../utils/style.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    required this.height,
+    required this.actionTitle,
+    this.showAction = true,
+    required this.actionOnPress,
+    this.title = '',
+  });
+
+  final double height;
+  final String actionTitle, title;
+  final bool showAction;
+  final VoidCallback actionOnPress;
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        title: Text(title),
+        titleTextStyle: Styles.textStyle18,
+        backgroundColor: kPrimaryColor,
+        iconTheme: const IconThemeData(color: Colors.white),
+        elevation: 3,
+        toolbarHeight: height,
+        actions: [
+          showAction
+              ? GestureDetector(
+                  onTap: actionOnPress,
+                  child: Row(
+                    children: [
+                      Text(
+                        actionTitle,
+                        style: Styles.textStyle18.copyWith(color: Colors.white),
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 18,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                    ],
+                  ),
+                )
+              : const SizedBox(),
+        ]);
+  }
+}
